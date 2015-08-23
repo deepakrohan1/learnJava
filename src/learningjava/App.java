4a5db4173f9b8d189060365bee1033e8c436ee2e
Deepak Rohan <deepakrohan@hotmail.com>
2015-08-23 13:08:35 -0400

    Array List Iterator

--- src/learningjava/App.java
+++ src/learningjava/App.java
@@ -1,5 +1,8 @@
 package learningjava;
 
+import java.util.Collections;
+import java.util.Random;
+
 public class App {
 
 	public App() {
@@ -8,17 +11,34 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		HelloArrayList hello = new HelloArrayList();
-		hello.addContent();
-		hello.names.add("Shiva G");
+		HelloArrayList hello = new HelloArrayList(); //Creating an object of the other class
+		hello.addContent(); // Accessing the methods of the other class
+		hello.names.add("Shiva G"); //Accessing the variables of the class intantiated
 		hello.students.add("Deepak Rohan");
-		
 		hello.students.add("Deepak Rohan");
 		hello.students.add("Sekar Rohan");
 		hello.students.add("Shiva G");
+		hello.students.set(1, "Deepu");//Set in ArrayList
+		hello.students.add(0,"Ganesh");
+		
 		
 		hello.listDisplay();
+		System.out.println("-------");
+		System.out.println(hello.students.get(1));//Gets the name at the Index
 		System.out.println(hello.names.equals(hello.students));
+		System.out.println("++++++++");
+		
+		Collections.shuffle(hello.students,new Random());
+		System.out.println(hello.students);
+		System.out.println("************");
+		hello.iterDisplay();
+		System.out.println("^^^^^^^^^^^^^");
+		hello.iterReverseDisplay();
+		System.out.println("#############");
+		hello.posDisplay();
+		System.out.println("Sorted");
+		Collections.sort(hello.students); //Sorts the list 
+		System.out.println(hello.students);
 	}
 
 }
--- src/learningjava/HelloArrayList.java
+++ src/learningjava/HelloArrayList.java
@@ -1,29 +1,61 @@
 package learningjava;
+
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.ListIterator;
 
 public class HelloArrayList {
-	ArrayList <String> names = new ArrayList<String>();
-	ArrayList <String> students = new ArrayList<String>();
+	ArrayList<String> names = new ArrayList<String>();
+	ArrayList<String> students = new ArrayList<String>();
+
 	public HelloArrayList() {
 		// TODO Auto-generated constructor stub
-//		ArrayList <String> names = new ArrayList<String>();
+		// ArrayList <String> names = new ArrayList<String>();
 	}
-	
-		public void addContent(){
-			names.add("Deepak Rohan");
-			names.add("Deepak Rohan");
-			names.add("Sekar Rohan");
-			
-		}
-		public void listDisplay(){
-			for(String name:names){
-				System.out.println(name);
-	}
-			System.out.println("========");
-			for (String name:students){
-				System.out.println(name);
-			}
 
-}
+	public void addContent() {
+		names.add("Deepak Rohan");
+		names.add("Deepak Rohan");
+		names.add("Sekar Rohan");
+
+	}
+
+	public void listDisplay() {
+		for (String name : names) {
+			System.out.println(name);
+		}
+		System.out.println("========");
+		for (String name : students) {
+			System.out.println(name);
+		}
+	}
+	/**
+	 * When no parameter has been passed to listIterator() displays them 
+	 * in the same order
+	 */
+	public void iterDisplay(){
+		for(ListIterator<String> it = students.listIterator(); it.hasNext();){ //listIterator doesnt take parameters
+			String t = it.next();  //Displays the contents in the same order
+			System.out.println(t);
+		}
+	}
+	/**
+	 * Reverse the order and display the contents. it checks for the previous and displays
+	 * them in that order
+	 */
+	public void iterReverseDisplay(){
+		for(ListIterator<String> it = students.listIterator(students.size());it.hasPrevious();){
+			String t = it.previous();
+			System.out.println(t);
+		}
+	}
+	/**
+	 * Displays the value from the particular index "1" in the below case
+	 */
+	public void posDisplay(){
+		for(ListIterator<String> it = students.listIterator(1);it.hasNext();){
+			String t = it.next();
+			System.out.println(t);
+		}
+	}
 }
