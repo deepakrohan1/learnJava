23582a1891f35d3d7046cf6dd17de1e2ba3e0cd7
Deepak Rohan <deepakrohan@hotmail.com>
2015-08-25 09:48:30 -0400

    Method overloading use of this key word

--- src/learningjava/Bicycle.java
+++ src/learningjava/Bicycle.java
@@ -8,14 +8,28 @@ int speed = 0, gear = 1;
 		this.speed = speed;
 		this.gear = gear;
 	}
-	
+	/**
+	 * Method Overloading speedUp with different parameters
+	 * @param increment
+	 */
 	public void speedUp(int increment){
 		this.speed = this.speed + increment;
 	}
 	
-	public void speedUpwith(int increment){
-		speed = speed + increment;
+	public void speedUp(){
+		this.speed = this.speed + 1;
 	}
+	/**
+	 * Signature of below method is speedUp(int, int)
+	 * @param increment
+	 * @param decrement
+	 */
+	public void speedUp(int increment, int decrement){
+		this.speed = this.speed + increment - decrement;
+	}
+	/**
+	 * Use of static method
+	 */
 	@Override
 	public String toString() {
 		return "Bicycle [speed=" + speed + ", gear=" + gear + "]";
--- src/learningjava/BicycleApp.java
+++ src/learningjava/BicycleApp.java
@@ -8,9 +8,18 @@ public class BicycleApp {
 	
 	public static void main(String[] args){
 		Bicycle bicycle = new Bicycle(100, 4);
-//		bicycle.speedUp(5);
-		bicycle.speedUpwith(5);
+		bicycle.speedUp(5);
 		System.out.println(bicycle.toString());
+		//Two ways to do this 
+		//1. Create a point object and then pass it to the static method
+		Point point = new Point(2, 0);
+		//Since it returns double
+		double x = Point.calculateDistance(point);
+		System.out.println(x);
+		//2.Create a point directly in the method
+		double y = Point.calculateDistance(new Point(1,0));
+		System.out.println(y);
+		
 	}
 
 }
